0d8da0d54cc17e394c22e6b2640d555677a5b20e
Brunomartins11 <brunocmartins@live.com.pt>
2020-03-27 14:37:04 +0000

    Add / route

--- src/main.go
+++ src/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/countryData", getCountryOverallDataHandler)
 	http.HandleFunc("/overallData", getOverallDataHandler)
 
 	// start the server on port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
